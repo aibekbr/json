990c9555fb623ba0a3d789f7b0b3c0af28046b8d
Aibek Shabdanaliev <Aibek Shabdanaliev>
2021-04-22 00:10:00 +0600

    updated

--- http_request.py
+++ http_request.py
@@ -14,9 +14,6 @@ from_currency = input("Select from curency: ")
 to_currency = input("Select to currency: ")
 amount = int(input("Enter an amount: "))
 
-rate_f_currency = rates.get(from_currency)
-rate_t_currency = rates.get(to_currency)
-
-result = amount / rate_f_currency * rate_t_currency
+result = amount / rates.get(from_currency) * rates.get(to_currency)
 
 print(f"{amount} {from_currency} = {result} {to_currency}")
