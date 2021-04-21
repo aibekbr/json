c1c2d3f410963ea0ac79e45eb8436ac62b020965
Aibek Shabdanaliev <Aibek Shabdanaliev>
2021-04-22 00:08:06 +0600

    updated

--- http_request.py
+++ http_request.py
@@ -3,20 +3,20 @@ import json
 
 r = requests.get("https://api.ratesapi.io/api/latest?symbols")
 data = json.loads(r.text)
-rates=data.get("rates")
+rates = data.get("rates")
 
-list_n=list(rates.keys())
+list_n = list(rates.keys())
 
 print(list_n)
 print("--------")
 
-from_currency=input("Select from curency: ")
-to_currency=input("Select to currency: ")
-amount=int(input("Enter an amount: "))
+from_currency = input("Select from curency: ")
+to_currency = input("Select to currency: ")
+amount = int(input("Enter an amount: "))
 
-result=amount/rates.get(from_currency)*rates.get(to_currency)
+rate_f_currency = rates.get(from_currency)
+rate_t_currency = rates.get(to_currency)
+
+result = amount / rate_f_currency * rate_t_currency
 
 print(f"{amount} {from_currency} = {result} {to_currency}")
-
-
-    
