b2d379c1e3ccde9ec54d51f3706e5f28f558172b
Aibek Shabdanaliev <Aibek Shabdanaliev>
2021-04-22 00:02:39 +0600

    updated

--- http_request.py
+++ http_request.py
@@ -10,13 +10,13 @@ list_n=list(rates.keys())
 print(list_n)
 print("--------")
 
-from_currency=input("Enter from curency: ")
-to_currency=input("Enter to currency: ")
+from_currency=input("Select from curency: ")
+to_currency=input("Select to currency: ")
 amount=int(input("Enter an amount: "))
 
 result=amount/rates.get(from_currency)*rates.get(to_currency)
 
-print(f"{amount} {from_currency} =",(result),f" {to_currency}")
+print(f"{amount} {from_currency} = {(result)} {to_currency}")
 
 
     
