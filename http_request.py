a10b1adf8d4d0eceed03c16c3b6c3d37fc417e25
Aibek Shabdanaliev <Aibek Shabdanaliev>
2021-04-21 23:34:48 +0600

    currency calculator

---
+++ http_request.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+r = requests.get("https://api.ratesapi.io/api/latest?symbols")
+data = json.loads(r.text)
+rates=data.get("rates")
+# usd=rates.get("USD")
+# print(100/usd)
+list_n=list(rates.keys())
+
+print(list_n)
+print("--------")
+from_currency=input("Enter from curency: ")
+
+print(list_n)
+print("--------")
+to_currency=input("Enter to currency: ")
+
+
+amount=int(input("Enter an amount: "))
+print("From currency: ",from_currency )
+print("To currency: ", to_currency)
+print(f"{amount} {from_currency} =",(amount/rates.get(from_currency)*rates.get(to_currency)),f" {to_currency}")
+
+
+    
